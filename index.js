d4739698250da39628e1619220ad7e50aec34b99
James Wright <james.wright42@outlook.com>
2018-02-23 19:57:32 +1100

    Remove changes not explained in a commit
    A previous commit changed two variables from being declared with let to being
    declared with const. Whilst I stand by this being the correct action, it was
    done without being mentioned in the commit, and so shouldn't have been
    commited. This commit undoes those changes, such that the git history will
    more accurately record what has been done in the repository.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require('./server')
+let server = require('./server')
 const Drones = require('./drones')
 const config = require('./config.json')
-const drones = new Drones({
+let drones = new Drones({
   log: config.log
 })
 
